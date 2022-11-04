934bade6d17c420f1179663f947823112f82a437
Diego <diegoccpinto@gmail.com>
2022-11-04 11:50:24 -0300

    ajuste responsive ainda

---
+++ myScript.js
@@ -0,0 +1,42 @@
+const payment = document.getElementById("payment_account");
+const approved = document.getElementById("approved_payment");
+
+const buttonPayment = document.getElementById("button_conf");
+const buttonApproved = document.getElementById("button_content");
+
+const nome = document.getElementById("text_name");
+const numberCard = document.getElementById("number_card");
+const numMonth = document.getElementById("month");
+const numYear = document.getElementById("year");
+const cvc = document.getElementById("code_verification_card");
+
+// const typePayment = document.getElementById("campTypePayment");
+// const cardValid = document.getElementById("campCardValid");
+
+buttonPayment.addEventListener("click", () => {
+
+
+    if (nome.value == '' || numberCard.value == '' || numMonth.value == '' || numYear.value == '' || cvc.value == '') {
+        return;
+    }
+    else {
+        if (payment) {
+            payment.style.display = "none";
+            approved.style.display = "grid";
+        }
+        else {
+            payment.style.display = "grid";
+        }
+        console.log("Teste1");
+    }
+
+
+    buttonApproved.addEventListener("click", () => {
+        if (approved) {
+            approved.style.display = "none";
+            payment.style.display = "grid";
+        }
+
+    })
+})
+
